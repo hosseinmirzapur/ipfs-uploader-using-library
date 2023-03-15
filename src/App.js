f917c6836e2ec1c7db351cc5e2541e4dd6dd3219
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-03-15 17:26:15 +0330

    took care of storage and all the files it can hold

--- src/App.js
+++ src/App.js
@@ -5,44 +5,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Gun from 'gun'
 
 const App = () => {
+    // Variables
     const [ipfs, setIpfs] = useState(null)
-    const [gun, setGun] = useState(null)
     const [directory, setDirectory] = useState([])
     const [uploadedFiles, setUploadedFiles] = useState([])
     const [loading, setLoading] = useState(false)
+    const [gun, setGun] = useState(null)
 
-
+    // Methods
     const initialize = async () => {
         setLoading(true)
+
         const node = await IPFS.create()
         setIpfs(node)
 
-        const createdGun = Gun()
-        setGun(createdGun)
+        const gun = Gun()
+        setGun(gun)
 
-        createdGun.get('files').map(item => setUploadedFiles([...uploadedFiles, item]))
+        const files = gun.get('files')
+        const fileArray = []
+        await files.map().on(file => {
+            fileArray.push({
+                cid: file?.cid
+            })
+        })
+        setUploadedFiles(fileArray)
+
         setLoading(false)
     }
-    const addToGun = (cid) => {
+
+    const addToGun = async (cid) => {
         const files = gun.get('files')
-        files.set({
-            cid: cid.toString()
-        })
+        const data = {cid: cid.toString()}
+        files.set(data)
+        await setUploadedFiles([...uploadedFiles, data])
     }
 
     const handleClearStorage = () => {
         localStorage.clear()
         setUploadedFiles([])
+        gun.get('files').put(null)
     }
-
 
     const addFileToDirectory = async (file) => {
         setLoading(true)
         const addedFile = await ipfs.add(file)
         setDirectory([...directory, addedFile])
-        addToGun(addedFile.cid)
         setLoading(false)
     }
+
     const handleLinkClick = async (cid) => {
         setLoading(true)
         const a = document.createElement('a')
@@ -53,7 +64,6 @@ const App = () => {
         a.remove()
         setLoading(false)
     }
-
 
     return (
         <Container className="mt-4">
@@ -75,10 +85,12 @@ const App = () => {
                                             <Form>
                                                 <Form.Group controlId="formFile">
                                                     <Form.Label>
-                                                        Select a file to add to directory
+                                                        Select a file to add to IPFS
                                                     </Form.Label>
-                                                    <Form.Control type="file"
-                                                                  onChange={e => addFileToDirectory(e.target.files[0])}/>
+                                                    <Form.Control
+                                                        type="file"
+                                                        onChange={e => addFileToDirectory(e.target.files[0])}
+                                                    />
                                                 </Form.Group>
                                             </Form>
                                         </>
@@ -93,19 +105,31 @@ const App = () => {
                 </Row>
 
                 <Row className="mt-4">
+                    <div className={'d-flex justify-content-center text-center'}>
+                        <h3>{directory.length === 0 ? 'No files uploaded yet' : 'Files uploaded to IPFS:'}</h3>
+                    </div>
                     {
                         directory.map(file => (
-                            <Col md={4} key={file.cid.toString()}>
+                            <Col md={4} key={`${file.cid.toString()}-stuff`}>
                                 <Card>
                                     <Card.Body>
                                         <Card.Title>{file.path}</Card.Title>
                                         <Card.Subtitle className="mb-2 text-muted">{file.cid.toString()}</Card.Subtitle>
                                         <Card.Footer className={'d-flex flex-row gap-2'}>
-                                            <Button variant={'outline-success'}
-                                                    onClick={() => handleLinkClick(file.cid.toString())}>Show
-                                                on IPFS</Button>
+                                            <Button
+                                                variant={'outline-success'}
+                                                onClick={() => handleLinkClick(file.cid.toString())}
+                                            >
+                                                Show on IPFS
+                                            </Button>
                                             <Button variant={'outline-danger'} onClick={handleClearStorage}>
                                                 Clear Storage
+                                            </Button>
+                                            <Button
+                                                variant={'outline-primary'}
+                                                onClick={() => addToGun(file.cid.toString())}
+                                            >
+                                                Save File
                                             </Button>
                                         </Card.Footer>
                                         <Card.Text>{file.size} bytes</Card.Text>
@@ -119,17 +143,22 @@ const App = () => {
                 <hr/>
 
                 <Row className="mt-4">
-                    <h3 className={'d-flex justify-content-center'}>{uploadedFiles.length === 0 ? 'No uploaded files found' : 'Uploaded Files so far...'}</h3>
+                    <h3 className={'d-flex justify-content-center'}>
+                        {uploadedFiles.length === 0 ? 'No saved files found' : 'Saved files so far:'}
+                    </h3>
                     {
-                        uploadedFiles.map(file => (
-                            <Col md={4} key={file.cid.toString()}>
+                        uploadedFiles.map((file, index) => (
+                            <Col md={4} key={`uploaded-${index}`} className={'mb-3'}>
                                 <Card>
                                     <Card.Body>
-                                        <Card.Text className="mb-2 text-muted">{file.cid.toString()}</Card.Text>
+                                        <Card.Text className="mb-2 text-muted">{file?.cid?.toString()}</Card.Text>
                                         <Card.Footer className={'d-flex flex-row gap-2'}>
-                                            <Button variant={'outline-success'}
-                                                    onClick={() => handleLinkClick(file.cid.toString())}>Show
-                                                on IPFS</Button>
+                                            <Button
+                                                variant={'outline-success'}
+                                                onClick={() => handleLinkClick(file?.cid?.toString())}
+                                            >
+                                                Show on IPFS
+                                            </Button>
                                             <Button variant={'outline-danger'} onClick={handleClearStorage}>
                                                 Clear Storage
                                             </Button>
